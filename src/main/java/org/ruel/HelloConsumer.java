b50f43fda37fb781e65d9c79fc0e683c62dfd25b
november_rue <novemberswimmer@gmail.com>
2018-01-18 06:17:34 -0600

    Fix issue on HelloConsumer

--- src/main/java/org/ruel/HelloConsumer.java
+++ src/main/java/org/ruel/HelloConsumer.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -20,7 +19,7 @@ public class HelloConsumer {
     private static Logger log = LoggerFactory.getLogger(HelloConsumer.class);
 
     @JmsListener(destination = HELLO_QUEUE)
-    public void receiveMessage(@Payload Hello hello, @Header MessageHeaders headers,
+    public void receiveMessage(@Payload Hello hello, @Headers MessageHeaders headers,
                                Message message, Session session){
         log.info("====================================================" + "\n" +
             "Headers =" + headers + "\n" +
